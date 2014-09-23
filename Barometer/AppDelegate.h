3ef3f9ac1eb3c0888c78dfc0357173f5ffe85d96
Montana Burr <montana@Montanas-MacBook-Pro.local>
2014-09-23 11:37:54 -0700

    Initial Commit

---
+++ Barometer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Barometer
+//
+//  Created by Montana Burr on 9/23/14.
+//  Copyright (c) 2014 Montana. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Barometer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Barometer
+//
+//  Created by Montana Burr on 9/23/14.
+//  Copyright (c) 2014 Montana. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
